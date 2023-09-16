c2757af6b3c72691ab83afae1863abcd0f70db06
Shaon shah <127724216+shaon36@users.noreply.github.com>
2023-09-16 21:52:04 +0600

    Add files via upload

---
+++ E_Area_of_a_Circle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double r,area;
+    double pi=3.141592653;
+    cin>>r;
+    area= pi*(r*r);
+    cout<<fixed<<setprecision(9)<<area<<endl;
+
+    return 0;
+}
