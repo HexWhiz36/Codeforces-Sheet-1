cfbad998b5a206cba87f6ba566c2f7bf8b40ce22
5H40N <shaonshah36@gmail.com>
2023-09-17 21:40:54 +0600

    Add files via upload

---
+++ J_Multiples.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a,b;
+    cin>>a>>b;
+    if(a>b)
+    {
+        if (a%b==0) cout<<"Multiples"<<endl;
+        else cout<<"No Multiples"<<endl;
+    }
+    else 
+    {
+        if (b%a==0) cout<<"Multiples"<<endl;
+        else cout<<"No Multiples"<<endl;
+    }
+    return 0;
+}
