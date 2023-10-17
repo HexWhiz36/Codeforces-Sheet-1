8370a43f4f0cc418a37f6ae405ac2530a65fa428
5H40N <shaonshah36@gmail.com>
2023-10-17 13:16:08 +0600

    Add files via upload

---
+++ P_First_digit.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    
+   if((a/1000)%2==0) cout<<"EVEN"<<endl;
+   else cout<<"ODD"<<endl;
+    return 0;
+}
