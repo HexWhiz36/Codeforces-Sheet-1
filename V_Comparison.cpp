3570923fc46819a52478b29eca0d1e218436eccb
5H40N <shaonshah36@gmail.com>
2023-10-21 01:22:09 +0600

    Add files via upload

---
+++ V_Comparison.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    char s;
+    cin>>a>>s>>b;
+    if(s=='=')
+    {
+        if(a==b) cout<<"Right"<<endl;
+        else cout<<"Wrong"<<endl;
+    }
+    else if(s=='>')
+    {
+        if(a>b) cout<<"Right"<<endl;
+        else cout<<"Wrong"<<endl;
+    }
+    else if(s=='<')
+    {
+        if(a<b) cout<<"Right"<<endl;
+        else cout<<"Wrong"<<endl;
+    }
+    return 0;
+}
